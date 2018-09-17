be92c9ca8c3f012ef68c30ff2ed90c62261ee9be
Musa Jahun <msjahun@live.com>
2018-09-17 21:28:25 +0300

    Wrote the code for the firebaseactivity and also a notification test in the onclick listener of the test button

---
+++ app/src/main/java/com/example/dc/pushnotificationsfirebasecm/FireBaseActivity.java
@@ -0,0 +1,77 @@
+package com.example.dc.pushnotificationsfirebasecm;
+
+import android.app.Notification;
+import android.app.NotificationChannel;
+import android.app.NotificationManager;
+import android.app.PendingIntent;
+import android.content.Context;
+import android.content.Intent;
+import android.os.Build;
+import android.support.annotation.RequiresApi;
+import android.support.v4.app.NotificationCompat;
+import android.support.v4.app.NotificationManagerCompat;
+import android.support.v4.app.RemoteInput;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
+
+import com.google.firebase.iid.FirebaseInstanceId;
+
+public class FireBaseActivity extends AppCompatActivity {
+
+    private static final String KEY_TEXT_REPLY = "key_text_reply";
+
+    private static final String  TAG = "MainActivity";
+    @RequiresApi(api = Build.VERSION_CODES.O)
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_fire_base);
+// Key for the string that's delivered in the action's intent.
+
+
+
+        Button btn_test = (Button) findViewById(R.id.btn_text);
+
+        btn_test.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                String token = FirebaseInstanceId.getInstance().getToken();
+                Log.d(TAG, "onClick: Token: "+token);
+                Toast.makeText(FireBaseActivity.this, token, Toast.LENGTH_LONG).show();
+
+                showNotification("Black pink","Lisa, jii soo, and others");
+
+
+
+
+            }
+        });
+    }
+
+    void showNotification(String title, String content) {
+        NotificationManager mNotificationManager =
+                (NotificationManager) getSystemService(Context.NOTIFICATION_SERVICE);
+        if (android.os.Build.VERSION.SDK_INT >= android.os.Build.VERSION_CODES.O) {
+            NotificationChannel channel = new NotificationChannel("default",
+                    "YOUR_CHANNEL_NAME",
+                    NotificationManager.IMPORTANCE_DEFAULT);
+            channel.setDescription("YOUR_NOTIFICATION_CHANNEL_DISCRIPTION");
+            mNotificationManager.createNotificationChannel(channel);
+        }
+        NotificationCompat.Builder mBuilder = new NotificationCompat.Builder(getApplicationContext(), "default")
+                .setSmallIcon(R.mipmap.ic_launcher) // notification icon
+                .setContentTitle(title) // title for notification
+                .setContentText(content)// message for notification
+
+                .setAutoCancel(true); // clear notification after click
+        Intent intent = new Intent(getApplicationContext(), FireBaseActivity.class);
+        PendingIntent pi = PendingIntent.getActivity(this, 0, intent, PendingIntent.FLAG_UPDATE_CURRENT);
+        mBuilder.setContentIntent(pi);
+        mNotificationManager.notify(0, mBuilder.build());
+    }
+}
